b2cce765d18291b972a34161d01602c1f1deb2cb
NM <134628965+mei3011@users.noreply.github.com>
2024-03-11 10:13:49 +0700

    Delete pkg-mirrors directory

--- pkg-mirrors/DownloadRPMs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-NPS_RPM_ADDITIONS="/etc/nps-rpm-additions"
-
-while read rpmaddition;
-do
-    echo "Downloading RPM $rpmaddition"
-    sudo wget $rpmaddition
-done < $NPS_RPM_ADDITIONS
