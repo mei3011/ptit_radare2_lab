d21428038b3c1cd2465624f6e1ac9e7faa9822a7
NM <134628965+mei3011@users.noreply.github.com>
2024-03-11 10:14:40 +0700

    Delete headless-lite directory

--- headless-lite/doupdate.sh
+++
@@ -1,7 +0,0 @@
-cd $LABTAINER_DIR/..
-wget --quiet https://github.com/mfthomps/Labtainers/releases/latest/download/labtainer.tar -O labtainer.tar
-echo "doing update of labtainer" >/tmp/update.log
-sync
-cd ..
-tar xf labtainer/labtainer.tar --keep-newer-files --warning=none
-sleep 1
--- headless-lite/get-results.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Retrieving Labtainer results from $1"
-mkdir -p ~/labtainer_xfer
-scp -r labtainer@$1:~/headless-labtainers/labtainer_xfer/* ~/labtainer_xfer/
